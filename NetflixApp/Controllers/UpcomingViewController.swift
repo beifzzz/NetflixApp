e66816523b4a169728d73da188a41410f65533e0
Bek <bekknop@gmail.com>
2023-03-05 19:50:19 +0600

    adding configure for UpcomingController

--- NetflixApp/Controllers/UpcomingViewController.swift
+++ NetflixApp/Controllers/UpcomingViewController.swift
@@ -2,7 +2,13 @@ import UIKit
 
 class UpcomingViewController: UIViewController {
 
-    private let 
+    var titles: [Movie] = [Movie]()
+    
+    private let upcomingTable: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
     
     
     override func viewDidLoad() {
@@ -12,9 +18,48 @@ class UpcomingViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationItem.largeTitleDisplayMode = .always
         
+        view.addSubview(upcomingTable)
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
         
-        
-        
-        
+        fetchUpcoming()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
+    }
+    
+    private func fetchUpcoming() {
+        APICaller.shared.getUpcomingMovies { result in
+            switch result {
+            case .success(let titles):
+                self.titles = titles
+                DispatchQueue.main.async {
+                    self.upcomingTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+
+
+extension UpcomingViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = titles[indexPath.row].title ?? titles[indexPath.row].original_title ?? "Unknown"
+        return cell
+    }
+}
+extension UpcomingViewController: UITableViewDelegate {
+    
+    
 }
